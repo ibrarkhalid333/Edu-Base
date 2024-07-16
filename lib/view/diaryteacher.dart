8cc347cb1cfe996bf70a72d8902b1758e5e2da44
Ibrar Khalid <ibrarkhalidhng@gmail.com>
2024-07-17 03:43:04 +0500

    Changes committed

---
+++ lib/view/diaryteacher.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DiaryForTeacher extends StatefulWidget {
+  const DiaryForTeacher({super.key});
+
+  @override
+  State<DiaryForTeacher> createState() => _DiaryForTeacherState();
+}
+
+class _DiaryForTeacherState extends State<DiaryForTeacher> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
